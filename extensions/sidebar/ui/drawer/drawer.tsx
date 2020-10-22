54a2b1fd45e56e0e2b301083470fd686f0377682
oded <oded@bit.dev>
2020-10-22 15:59:08 +0300

    removed opening of '/' from drawer now that we have the overview link

--- extensions/sidebar/ui/drawer/drawer.tsx
+++ extensions/sidebar/ui/drawer/drawer.tsx
@@ -1,5 +1,4 @@
 import { Icon } from '@teambit/evangelist.elements.icon';
-import { NavLink } from '@teambit/react-router';
 import classNames from 'classnames';
 import React from 'react';
 
@@ -16,14 +15,11 @@ export function DrawerUI({ drawer, className, isOpen, onToggle, ...rest }: Drawe
   if (!drawer) return null;
   return (
     <div {...rest} className={classNames(styles.drawer, className)}>
-      <div className={classNames(styles.drawerName, { [styles.open]: isOpen })}>
-        <div onClick={onToggle}>
+      <div onClick={onToggle} className={classNames(styles.drawerName, { [styles.open]: isOpen })}>
+        <div>
           <Icon className={classNames(styles.arrow, { [styles.collapsed]: !isOpen })} of="fat-arrow-down" />
           <span>{drawer.name}</span>
         </div>
-        <NavLink href="/" className={styles.link}>
-          <Icon of="comps" />
-        </NavLink>
       </div>
 
       <div className={classNames(styles.drawerContent, { [styles.open]: isOpen })}>
